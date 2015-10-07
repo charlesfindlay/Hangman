01a532e4e03bfb4f21bbf4f92cbf1aed29f1935b
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-10-07 13:42:41 -0400

    exact match case working

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -11,19 +11,39 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var wordToGuessLabel: UILabel!
+    @IBOutlet weak var userGuess: UITextField!
+    @IBOutlet weak var pastGuesses: UILabel!
     
     
     
     let wordList = "computer"
-    
+    var hiddenWord: String!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupGame()
     }
+    
+    
+    func setupGame() {
+        hiddenWord = wordList
+        pastGuesses.text = "Past Guesses"
+    }
+    
+    
+    
 
     
+    @IBAction func userMakesAGuess(sender: AnyObject) {
+        if userGuess.text == hiddenWord {
+            print("You Win!")
+        }
+        
+        
+    }
 
 }
 
